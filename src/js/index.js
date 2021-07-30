25779e8e25de6e9a42a10bce4d9852b67b8b175e
Груздева Светлана <gruzdeva.s@outlook.com>
2021-07-30 23:15:13 +0300

    Начало

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+jQuery(function() {
+  // jQuery(".owl-carousel").owlCarousel({
+  //   nav: true,
+  //   dots: false,
+  //   navText: '',
+  //   margin: 50,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+
+  //     767: {
+        
+  //       items: 2,
+  //     },
+
+  //     992: {
+  //       items: 3,
+  //     }
+  //   }
+  // });
+
+  jQuery('.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:4
+})
+});
